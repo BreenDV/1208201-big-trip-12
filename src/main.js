0604125c0f87a6480c620515492c44091fba9f9e
Дмитрий <breendv92@mail.ru>
2020-08-08 18:05:56 +0400

    Дубль 2

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@
 
 const TASK_COUNT = 3;
 
-const sitePageHeader = document.querySelector('.page-header');
+const sitePageHeader = document.querySelector(`.page-header`);
 
-const sitePageTripMain = sitePageHeader.querySelector('.trip-main');
+const sitePageTripMain = sitePageHeader.querySelector(`.trip-main`);
 
-const siteControls = sitePageHeader.querySelector('.trip-main__trip-controls');
+const siteControls = sitePageHeader.querySelector(`.trip-main__trip-controls`);
 
-const siteControlsHeadlines = siteControls.querySelectorAll('h2');
+const siteControlsHeadlines = siteControls.querySelectorAll(`h2`);
 
-const sitePageMain = document.querySelector('.page-body__page-main');
+const sitePageMain = document.querySelector(`.page-body__page-main`);
 
-const siteTripEvents = sitePageMain.querySelector('.trip-events');
+const siteTripEvents = sitePageMain.querySelector(`.trip-events`);
 
 const createSiteRouteInformationTemplate = () => {
   return (
@@ -424,34 +424,34 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-let section = document.createElement('section');
-section.className = 'trip-main__trip-info  trip-info';
+let section = document.createElement(`section`);
+section.className = `trip-main__trip-info  trip-info`;
 
 sitePageTripMain.insertBefore(section, siteControls);
 
-let newSection = sitePageTripMain.querySelector('.trip-main__trip-info');
+let newSection = sitePageTripMain.querySelector(`.trip-main__trip-info`);
 
-render(newSection, createSiteRouteInformationTemplate(), 'beforeend');
+render(newSection, createSiteRouteInformationTemplate(), `beforeend`);
 
-render(newSection, createSiteRoutecostTemplate(), 'beforeend');
+render(newSection, createSiteRoutecostTemplate(), `beforeend`);
 
-render(siteControlsHeadlines[0], createSiteMenuTemplate(), 'afterend');
+render(siteControlsHeadlines[0], createSiteMenuTemplate(), `afterend`);
 
-render(siteControlsHeadlines[1], createSiteFiltersTemplate(), 'afterend');
+render(siteControlsHeadlines[1], createSiteFiltersTemplate(), `afterend`);
 
-render(siteTripEvents, createSiteSortingTemplate(), 'beforeend');
+render(siteTripEvents, createSiteSortingTemplate(), `beforeend`);
 
-render(siteTripEvents, createSiteFormEventTemplate(), 'beforeend');
+render(siteTripEvents, createSiteFormEventTemplate(), `beforeend`);
 
-let list = document.createElement('ul');
-list.className = 'trip-days';
+let list = document.createElement(`ul`);
+list.className = `trip-days`;
 
 siteTripEvents.appendChild(list);
 
-let newList = siteTripEvents.querySelector('.trip-days');
+let newList = siteTripEvents.querySelector(`.trip-days`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-render(newList, createSiteWaypoint(), 'beforeend');
+  render(newList, createSiteWaypoint(), `beforeend`);
 }
 
 
